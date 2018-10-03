5bfd6150dbfc034eec3ed70140b57062e8076c73
Vyapak Khare <39800707+vyapak1209@users.noreply.github.com>
2018-10-03 17:02:02 +0530

    Comments

--- src/reducers/ImageReducer.js
+++ src/reducers/ImageReducer.js
@@ -5,16 +5,16 @@ export default (state = INITIAL_STATE, action) => {
   console.log(action)
   switch (action.type) {
     case SPELL_CHECK: 
-      return { ...state, spellCheck: action.payload}
+      return { ...state, spellCheck: action.payload} // Spell check prompt if spellcheck_info object is not null
     case NOT_FOUND: 
-      return { ...state, data: null, error: action.payload}
+      return { ...state, data: null, error: action.payload}  // Data object is on 0 length since query not understood
     case IMAGES_FETCHED:
       if (state.data === null ){
-        return { ...state, data: action.payload, error: null }
+        return { ...state, data: action.payload, error: null }  // First time data is loaded. data is initially null
       }else if(action.text === QUERY_CHANGED){
-        return {...state, data: action.payload, error: null}
+        return {...state, data: action.payload, error: null}  // Refresh the array object with new data since query changed
       }else{
-        return {...state, data: state.data.concat(action.payload), error: null}
+        return {...state, data: state.data.concat(action.payload), error: null}     // Concat lazy loaded data if query is unchanged
       }
     default:
       return state;
